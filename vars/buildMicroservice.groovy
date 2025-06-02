2f16a1fbc7945c23beb77fc5389be8b3b637cabf
RzemMoahemd <mohamed.rzem@esprit.tn>
2025-06-02 11:53:54 +0100

    Initial shared lib 3

--- vars/buildMicroservice.groovy
+++ vars/buildMicroservice.groovy
@@ -25,15 +25,22 @@ def call(Map config) {
             }
             
             stage('Build Docker Image') {
-                steps {
-                    dir(PROJECT_PATH) {
-                        script {
-                            sh "docker --version"
-                            docker.build("${IMAGE_NAME}:latest")
-                        }
-                    }
-                }
+    steps {
+        dir(PROJECT_PATH) {
+            script {
+                // Vérification du socket Docker
+                sh '''
+                    echo "Vérification de l'accès à Docker:"
+                    ls -l /var/run/docker.sock
+                    docker info
+                '''
+                
+                // Construction manuelle de l'image
+                sh "docker build -t ${IMAGE_NAME}:latest ."
             }
+        }
+    }
+}
             
             stage('Push to Nexus') {
                 steps {
