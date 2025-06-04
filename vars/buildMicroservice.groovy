97f89e884c12626e1d24bab85059d031a282bbed
RzemMoahemd <mohamed.rzem@esprit.tn>
2025-06-04 15:13:13 +0100

    Initial shared lib 3

--- vars/buildMicroservice.groovy
+++ vars/buildMicroservice.groovy
@@ -3,6 +3,9 @@ def call(Map config) {
         agent any
         
         environment {
+            HTTP_PROXY = "http://squid-proxy.jenkins.svc.cluster.local:3128"
+            HTTPS_PROXY = "http://squid-proxy.jenkins.svc.cluster.local:3128"
+            NO_PROXY = "localhost,127.0.0.1,.svc.cluster.local"
             SERVICE_NAME = "${config.serviceName}"
             IMAGE_NAME = "${config.imageName}"
             PROJECT_PATH = "${config.projectPath}"
@@ -11,13 +14,31 @@ def call(Map config) {
         
         
         stages {
-            stage('Wait for DNS ready') {
+            stages {
+        stage('Wait for DNS ready') {
             steps {
-                sh '''
-                    for i in {1..5}; do
-                        nslookup github.com && nslookup repo.maven.apache.org && break || sleep 5
-                    done
-                '''
+                script {
+                    // Install required network tools
+                    sh '''
+                        apt-get update
+                        apt-get install -y dnsutils iputils-ping curl
+                    '''
+                    
+                    // Verify DNS resolution with retries
+                    int maxTries = 5
+                    int waitTime = 10
+                    for(int i = 0; i < maxTries; i++) {
+                        try {
+                            sh 'nslookup repo.maven.apache.org'
+                            sh 'ping -c 3 repo.maven.apache.org'
+                            echo "DNS verification successful"
+                            break
+                        } catch (Exception e) {
+                            echo "DNS not ready yet. Attempt ${i+1}/${maxTries}"
+                            sleep(waitTime)
+                        }
+                    }
+                }
             }
         }
 
