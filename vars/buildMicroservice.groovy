ac3a4fe02c7166ccee59efbd4c556c784faf253c
RzemMoahemd <mohamed.rzem@esprit.tn>
2025-06-24 16:48:58 +0100

    ajout de deployement

--- vars/buildMicroservice.groovy
+++ vars/buildMicroservice.groovy
@@ -340,16 +340,16 @@ def call(Map config) {
                     script {
                         dir("${PROJECT_PATH}/k8s") {
 
-                             withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
-                                sh """
-                                    kubectl apply -f deployment.yaml --kubeconfig="\$KUBECONFIG --validate=false"
-                                """
-                            }
+                            //  withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
+                            //     sh """
+                            //         kubectl apply -f deployment.yaml --kubeconfig="\$KUBECONFIG --validate=false"
+                            //     """
+                            // }
                             //sh "unset http_proxy"
                             //sh "unset https_proxy"
                            // sh "kubectl apply -f deployment.yaml --kubeconfig=${KUBECONFIG} --context=master-node"
                             //sh "kubectl --server=https://192.16.0.233:6443 apply -f deployment.yaml"
-                            //sh "kubectl --kubeconfig=\${KUBECONFIG} apply -f deployment.yaml --validate=false"
+                            sh "kubectl --kubeconfig=\${KUBECONFIG} apply -f deployment.yaml --validate=false"
                             //sh "kubectl apply -f deployment.yaml --validate=false"
                             sh "kubectl rollout status deployment/${SERVICE_NAME} --timeout=300s"
                             sh "kubectl get pods -l app=${SERVICE_NAME}"
